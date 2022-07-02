b7b08e5973487a71cbaef5b50ab4545702bb7793
IrynaZhuk <irynazh94@gmail.com>
2022-07-03 01:13:04 +0300

    without styles

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ let lightbox;
 
 const form = document.querySelector('.search-form');
 const loadMoreBtn = document.querySelector('.load-more');
-const gallary = document.querySelector('.gallary');
+const gallery = document.querySelector('.gallery');
 
 form.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick)
 
 function onFormSubmit(evt) {
     evt.preventDefault();
@@ -33,12 +34,59 @@ function firstRenderMarkupGallery (searchIteam) {
             return Notify.info ('Unfortunately, there are not any matches. Try again, please') 
         }
 
-        if (response.hits.length < 40) {
-loadMoreBtn.classList.add('display-none')
-        }
+//         if (response.hits.length < 40) {
+// loadMoreBtn.classList.add('display-none')
+//         }
 
-        gallery.innerHTML = '';
+        // gallery.innerHTML = '';
       Notify.info(`We have found ${response.totalHits} images.`);
-      gallery.insertAdjacentHTML('beforeend', makeMarkupGallery(response.hits));
-    })
+      gallery.insertAdjacentHTML('afterbegin', makeMarkupGallery(response.hits));
+    
+      if (response.hits.length === 40) {
+        loadMoreBtn.classList.remove('display-none')
+    }
+
+    lightbox = new SimpleLightbox('.gallery a');
+      scroll(0.2);
+    }).catch(error => console.log(error))
 }
+
+function onLoadMoreBtnClick () {
+    page +=1;
+    renderMarkupGallery(form.elements.searchQuery.value.trim());
+}
+
+function renderMarkupGallery(search) {
+    fetchImages(search, page)
+      .then(response => {
+        if (response.hits.length < 40) {
+          Notify.info(
+            "You've already reached the end of search results."
+          );
+          loadMoreBtn.classList.add('display-none');
+        }
+  
+        gallery.insertAdjacentHTML('beforeend', makeMarkupGallery(response.hits));
+  
+        lightbox.refresh();
+        scroll(2);
+      })
+      .catch(error => {
+        console.log(error);
+        Notify.info("You've already reached the end of search results.");
+        loadMoreBtn.classList.add('display-none');
+      });
+  }
+
+  function scroll(value) {
+    setTimeout(() => {
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+  
+      window.scrollBy({
+        top: cardHeight * value,
+        behavior: 'smooth',
+      });
+    }, 600);
+  }
